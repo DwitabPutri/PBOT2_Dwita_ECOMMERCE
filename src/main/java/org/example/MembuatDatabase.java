dc5dbd26313c53b1070188d1e44aeab2e371b660
DwitabPutri <putrisuastini22@gmail.com>
2023-05-30 16:40:07 +0800

    Membuat database baru yang akan digunakan

---
+++ src/main/java/org/example/MembuatDatabase.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MembuatDatabase {
+    public static void createNewDatabase(String fileName){
+        String url = "jdbc:sqlite:C:/sqlite/" + fileName;
+        try {
+            Connection conn = DriverManager.getConnection(url);
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("Database Berhasil Dibuat");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+        createNewDatabase("db_ecommerce_dwita.db");
+    }
+}
