e167661be542669c3a87c9e95643628b5fd34b1c
DwitabPutri <putrisuastini22@gmail.com>
2023-05-30 17:01:21 +0800

    Melakukan koneksi database ke dalam proyek

---
+++ src/main/java/org/example/ConnectDatabase.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.sql.DriverManager;
+import java.sql.*;
+public class ConnectDatabase {
+    private Connection connection;
+
+    public ConnectDatabase() {
+        try {
+            this.connection = null;
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:db_ecommerce_dwita.db");
+        } catch
+        (ClassNotFoundException | SQLException e) {
+            System.out.println(e + "");
+        }
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+}
